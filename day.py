40850c1195ef1e26be672502614e2b36611d937e
bonsoul <bonsoul24@gmail.com>
2025-03-06 22:45:50 +0300

    Day Four

--- day.py
+++ day.py
@@ -8,3 +8,16 @@ import matplotlib.pyplot as plt
 data = load_diabetes()
 df=pd.DataFrame(data=data, columns=data.feature_names)
 df['target'] = data.target
+df.shape
+
+
+#diplay information
+print(df.head)
+
+
+correlation_matrix = df.corr()
+
+plt.figure(figsize=(10,8))
+sns.heatmap(correlation_matrix, annot =True, cmap = "coolwarm")
+plt.title("Correlation matrix")
+plt.show()
